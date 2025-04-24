5a7f0ecc3ee7517456ab3b09e9514276334858e1
mikey <mikey@hey.com>
2025-04-24 16:46:35 -0700

    Nest Activities under Trips

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,51 +1,21 @@
 class ActivitiesController < ApplicationController
-  before_action :set_activity, only: %i[ show update destroy ]
+  before_action :set_trip, only: %i[ index ]
 
-  # GET /activities
   def index
-    @activities = Activity.all
+    @activities = @trip.activities
 
     render json: @activities
   end
 
-  # GET /activities/1
-  def show
-    render json: @activity
-  end
-
-  # POST /activities
-  def create
-    @activity = Activity.new(activity_params)
-
-    if @activity.save
-      render json: @activity, status: :created, location: @activity
-    else
-      render json: @activity.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /activities/1
-  def update
-    if @activity.update(activity_params)
-      render json: @activity
-    else
-      render json: @activity.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /activities/1
-  def destroy
-    @activity.destroy!
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_activity
-      @activity = Activity.find(params.expect(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def activity_params
-      params.expect(activity: [ :name, :trip_id ])
-    end
+  def set_trip
+    @trip = Trip.find(params[:trip_id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Trip not found" }, status: :not_found
+  end
+
+  def activity_params
+    params.require(:activity).permit(:name)
+  end
 end
